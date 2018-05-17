36073e5269aed4e27776dd0240626187c60e00e3
Jovian Andrew <jovian_andrew@hotmail.com>
2018-05-17 17:52:47 +0700

    initial commit

---
+++ src/views/Lobby.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="lobby">
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import { db } from '@/firebase.js'
+
+export default {
+  name: 'lobby',
+  firebase: {
+    players: db.ref('players'),
+    rooms: db.ref('rooms')
+  },
+  methods: {
+    
+  }
+}
+</script>
